2483c455f011e351e3de28fb90729af1dd6ea962
Nakul <nakulsiwach007@gmail.com>
2023-06-18 17:44:02 +0530

    ann

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import pickle
 
+import pandas as pd
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+data = pd.read_csv('Cleaned_data.csv')
+pipe = pickle.load(open('newRidgeModel.pkl', 'rb'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=5000)
